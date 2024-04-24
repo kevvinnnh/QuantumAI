39af182b3a2ea4f52aac31199844f2931b1d8ed9
Kevin <kh78@rice.edu>
2024-04-23 19:31:59 -0500

    added start

--- Stack/BackEnd/app.py
+++ Stack/BackEnd/app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from pages import intro, main
+from pages import start, intro, main
 
 # Set page configuration here, at the very top and only once
-st.set_page_config(page_title="My Streamlit App", page_icon=":rocket:", layout="wide")
+st.set_page_config(page_title="Quantum Computing Quiz", page_icon=":book:", layout="wide")
 
 # Define pages
-pages = [intro.show, main.show]
+pages = [start.show, intro.show, main.show]
 
 # Initialize session state for page navigation
 if 'page_index' not in st.session_state:
@@ -26,7 +26,7 @@ if st.session_state.page_index + 1 < len(pages):
     st.button("Next", on_click=next_page)
 
 if st.session_state.page_index - 1 >= 0:
-    st.button("Back to Intro", on_click=prev_page)
+    st.button("Back", on_click=prev_page)
 
 # Display the current page function
 pages[st.session_state.page_index]()
---
+++ Stack/BackEnd/pages/start.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+def show():
+    """Displays a sophisticated introduction page for the Quantum Computing Quiz."""
+    # Configure page settings
+
+    # Use columns to create a centered layout for the content
+    col1, col2, col3 = st.columns([1, 2, 1])
+
+    with col2:
+        # Add a welcoming header
+        st.title("Welcome to the Quantum Computing Quiz!")
+
+        # Introduction text styled with a modern look
+        st.markdown("""
+            <div style='text-align: justify;'>
+                    <br>
+                Learn the fundamentals of quantum computing and test your knowledge.
+                <br><br><br>
+                        This interactive quiz will guide you through the basic concepts of quantum computing.
+        Get ready to explore the fascinating world of qubits, superposition, and quantum entanglement
+        through a series of engaging questions.
+            </div>
+            """, unsafe_allow_html=True)
+
+        # Custom styles for a modern, clean look
+        st.markdown("""
+            <style>
+                html, body, [class*="css"] {
+                    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;  # Modern and readable font
+                    background-color: #f4f4f8;  # Soft background color for reduced eye strain
+                    color: #333366;  # Dark blue color for text for better readability
+                }
+
+            </style>
+            """, unsafe_allow_html=True)
